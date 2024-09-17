0f5d94a63749a13fa612a920d913a3c5d05b352f
Alex <a.gonzalez5@utp.edu.co>
2024-09-16 23:34:16 -0500

    Commit sin importancia

--- NavStack/screens/HomeScreen.js
+++ NavStack/screens/HomeScreen.js
@@ -5,18 +5,21 @@ export default function Screen1({ navigation }) {
   return(
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text style={{fontWeight: 'bold', fontSize: 20}}>Home</Text>
+
       <View style={{margin: 10}}> 
         <Button
           title="Ir a la pantalla 1"
           onPress={() => navigation.navigate('Screen1')}
         />
       </View>
+
       <View style={{margin: 10}}> 
         <Button
           title="Ir a la pantalla 2"
           onPress={() => navigation.navigate('Screen2')}
         />
       </View>
+      
       <View style={{margin: 10}}> 
         <Button
           title="Ir a la pantalla 3"
