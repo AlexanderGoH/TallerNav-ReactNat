5ea5fdf284f9a14acd962e0ec38f58169ce3564f
Alex <a.gonzalez5@utp.edu.co>
2024-09-16 22:38:35 -0500

    Eliminación de cosas innecesarias

--- NavStack/screens/HomeScreen.js
+++ NavStack/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Button, View, Text, StyleSheet } from 'react-native';
+import { Button, View, Text } from 'react-native';
 
 export default function Screen1({ navigation }) {
   return(
